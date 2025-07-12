c637a2e9547c8229f8d10a8ae5b6b2eedbf92f47
AbdulKus <73951988+AbdulKus@users.noreply.github.com>
2025-07-12 16:31:37 +0300

    Refactor TinyOLED library with buffer-based drawing

--- TinyOLED.h
+++ TinyOLED.h
@@ -6,7 +6,7 @@
 
 #define OLED_WIDTH 72
 #define OLED_HEIGHT 40
-#define OLED_PAGES 5
+#define OLED_PAGES (OLED_HEIGHT/8)
 
 class TinyOLED {
   public:
@@ -15,20 +15,20 @@ class TinyOLED {
     void clear();
     void fill(uint8_t pattern = 0xFF);
     void drawPixel(uint8_t x, uint8_t y, uint8_t color = 1);
-    void drawRect(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t fill = 0, uint8_t invert = 0);
-    void drawText(uint8_t x, uint8_t y, const char* text, uint8_t font = 0, uint8_t invert = 0);
+    void drawRect(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, bool fill = false, bool invert = false);
+    void drawText(uint8_t x, uint8_t y, const char* text, bool invert = false);
     void setCursor(uint8_t x, uint8_t y);
     void update();
     void testPattern();
-    
+
   private:
     uint8_t _address;
     uint8_t _cursorX;
     uint8_t _cursorY;
+    uint8_t _buffer[OLED_WIDTH * OLED_HEIGHT / 8];
     void sendCommand(uint8_t cmd);
     void sendData(uint8_t data);
-    void setColumnAddress(uint8_t start, uint8_t end);
-    void setPageAddress(uint8_t start, uint8_t end);
+    void drawChar(uint8_t x, uint8_t y, char ch, bool invert);
 };
 
 #endif
